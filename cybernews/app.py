8c83d700670f6134ed0004204d61408b67d4372e
Morgan Dunn <morgandunn@Morgans-MacBook-Pro.local>
2024-03-03 02:06:34 -0800

    minor fix to remove testing comment

--- cybernews/app.py
+++ cybernews/app.py
@@ -9,8 +9,6 @@ def index():
         user_input = request.form["user_input"]
         get_summary = AICyberNewsSummary(user_input)
         response = get_summary.summary()
-        # Simulate sending user_input to the backend (replace with your actual logic)
-        #response = f"You entered: {user_input}"
         return render_template("index.html", user_input=user_input, response=response)
     else:
         return render_template("index.html")
